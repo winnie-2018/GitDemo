743d42010faa9c9962f10b98e2db454ea8297158
winnie <halowinnie@gmail.com>
2021-09-08 21:51:28 +0800

    new changes is made here --- commit

---
+++ src/GitTest.java
@@ -0,0 +1,11 @@
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		System.out.println("New Test 1");
+		System.out.println("New Test 2");
+		System.out.println("New Test 3");
+
+	}
+
+}
