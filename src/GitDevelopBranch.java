81c0d8e8f22cb78ed11c5497413b235a8b9b5e92
winnie <halowinnie@gmail.com>
2021-09-09 21:07:03 +0800

    Architect development

---
+++ src/GitDevelopBranch.java
@@ -0,0 +1,16 @@
+
+public class GitDevelopBranch {
+
+	public static void main(String[] args) {
+		System.out.println("Develop Branch 1");
+		System.out.println("Develop Branch 1");
+		System.out.println("Develop Branch 1");
+
+		developBranch();
+	}
+	
+	static void developBranch() {
+		System.out.println("Develop Branch 1");
+	}
+
+}
